128828d467eb8f7ecb3ebc0166ec8648c33d1728
raimundlima <77973887+raimundlima@users.noreply.github.com>
2023-08-01 11:27:59 -0300

    algoritmos em python

---
+++ main.py
@@ -0,0 +1,6 @@
+# definindo a variavel nome
+nome = input ("digite o seu nome: ")
+# definindo a variavel idade 
+idade = input ("digite sua idade: ")
+# exibindo as informações obtidas 
+print ("bem vindo" , nome , "sua idade é:", idade )
