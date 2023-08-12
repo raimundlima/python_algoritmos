a89f4d484146eebe4b36f676ae6dbd49d7c8ca46
RAIMUNDO JUSTINO FILGUEIRA DE LIMA <a95120770@rn.senac.local>
2023-08-12 09:35:01 -0300

    exemplo de vetores em python

---
+++ vetores.py
@@ -0,0 +1,13 @@
+numeros = []
+maior = 0 
+
+for in range (5):
+    numeros int (input(f"Digite um número {n + 1}: "))
+    numeros.append(numero)
+
+numeros.sort.(reserve.True)
+
+maior = numeros[0]
+
+print(f"O maior número da lsita {numero} é {maior}")
+
